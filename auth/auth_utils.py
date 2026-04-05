ba8759c6f4ff660a7e906722b0a72f45c2934daf
Fellsing <tamirmingal@gmail.com>
2026-04-05 19:24:29 +0300

    build: fix yaml syntax #try4

--- auth/auth_utils.py
+++ auth/auth_utils.py
@@ -16,9 +16,9 @@ from models.models import UserDB
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+SECRET_KEY = os.getenv("SECRET_KEY", "insanely-giga-secre-key-monster")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 
 class Token(BaseModel):
