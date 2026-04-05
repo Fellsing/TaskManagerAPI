25551cd17bcf097a1639d14aeb0b32fd2b2b6e28
Fellsing <tamirmingal@gmail.com>
2026-04-05 19:27:31 +0300

    build: fix yaml syntax #try5

--- auth/auth_router.py
+++ auth/auth_router.py
@@ -8,7 +8,6 @@ import jwt
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pwdlib import PasswordHash
-from dotenv import load_dotenv
 from pydantic import BaseModel, EmailStr
 
 from sqlalchemy.orm import Session
@@ -17,15 +16,10 @@ from database import get_db
 from models.models import UserDB
 from schemas.users import UserCreate
 
-from auth.auth_utils import Token, authenticate_user, create_access_token, get_user
+from auth.auth_utils import Token, authenticate_user, create_access_token, get_user, SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 
 
 router = APIRouter()
-load_dotenv()
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
-
 
 @router.post("/auth/signin")
 async def login_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db:Annotated[Session, Depends(get_db)])-> Token:
